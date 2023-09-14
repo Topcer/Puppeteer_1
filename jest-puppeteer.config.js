a0575f2c21e0c6b3bc2e5ef8b02c3190239b6337
Topce <topce@icloud.com>
2023-09-15 00:05:24 +0700

    first commit

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    launch: {
+        headless: false,
+        slowMo: 350,
+        defaultViewport: null,
+        args: ['--start-maximized'] //— используем максимальный размер окна браузера
+      },
+    
+  };
+  
